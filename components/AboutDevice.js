ab77d89b06afd22b5479849fdb731f1cec69edfe
Sanjay Govind <sanjay.govind9@gmail.com>
2018-09-03 17:35:43 +1200

    First commit, working tabs and router

---
+++ components/AboutDevice.js
@@ -0,0 +1,39 @@
+import {Component} from "react";
+import {StyleSheet, Text, View} from "react-native";
+import React from "react";
+
+export default class AboutDevice extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {page: 'second'};
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.welcome}>
+                    Welcome to React Native
+                </Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
+});
---
+++ components/TabIconIon.js
@@ -0,0 +1,26 @@
+import {StyleSheet, Text, View} from "react-native";
+import React from "react";
+import Icon from "react-native-vector-icons/Ionicons"
+
+export default class TabIconIon extends React.Component {
+    render() {
+
+        /** some styling **/
+
+        return (
+            <View style={styles.container}>
+                <Icon name={this.props.iconName} color={this.props.focused?"#4F8EF7":this.props.tintColor} size={20} />
+            </View>
+        );
+    }
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    }
+});

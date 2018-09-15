62a1abd898c3618e3576a087da8d6e2d9e4d87a7
Sanjay Govind <govindsanj@myvuw.ac.nz>
2018-09-16 03:41:19 +1200

    Oh hey look a working keyboard (kill me now please)

--- src/ts/components/play-tones.tsx
+++ src/ts/components/play-tones.tsx
@@ -1,34 +1,87 @@
 import React, {Component} from "react";
-import {StyleSheet, Text, View} from "react-native";
+import {Dimensions, StyleSheet, Text, TouchableHighlight, TouchableOpacity, View} from "react-native";
+import {ScrollView} from "react-native-gesture-handler";
 
-export default class PlayTones extends Component {
+export default class PlayTones extends Component<{}, { calculatedKeyWidth: number }> {
 
-    render() {
-        return (
-            <View style={styles.container}>
-                <Text style={styles.welcome}>
-                    Welcome to React Native
-                </Text>
-            </View>
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {calculatedKeyWidth: 0}
+  }
+
+  onLayout = event => {
+    const {width} = event.nativeEvent.layout;
+    this.setState({calculatedKeyWidth: width});
+  };
+
+  render() {
+    let pianoKeys: [{ whiteKeyId: number, blackKeyId?: number }] = [
+      {whiteKeyId: 16},
+      {whiteKeyId: 18, blackKeyId: 17},
+      {whiteKeyId: 20, blackKeyId: 19},
+      {whiteKeyId: 21},
+      {whiteKeyId: 23, blackKeyId: 22},
+      {whiteKeyId: 25, blackKeyId: 24},
+      {whiteKeyId: 27, blackKeyId: 26},
+      {whiteKeyId: 28},
+      {whiteKeyId: 30, blackKeyId: 29},
+      {whiteKeyId: 32, blackKeyId: 31},
+      {whiteKeyId: 33},
+      {whiteKeyId: 35, blackKeyId: 34},
+      {whiteKeyId: 37, blackKeyId: 36},
+      {whiteKeyId: 39, blackKeyId: 38},
+      {whiteKeyId: 40},
+      {whiteKeyId: 42, blackKeyId: 41},
+      {whiteKeyId: 44, blackKeyId: 43},
+      {whiteKeyId: 45},
+      {whiteKeyId: 47, blackKeyId: 46},
+      {whiteKeyId: 49, blackKeyId: 48},
+      {whiteKeyId: 51, blackKeyId: 50},
+      {whiteKeyId: 52},
+      {whiteKeyId: 54, blackKeyId: 53},
+      {whiteKeyId: 56, blackKeyId: 55},
+      {whiteKeyId: 57},
+      {whiteKeyId: 59, blackKeyId: 58},
+      {whiteKeyId: 61, blackKeyId: 60},
+      {whiteKeyId: 63, blackKeyId: 62},
+      {whiteKeyId: 64}
+    ];
+    let keyboard = pianoKeys.map((fn) => {
+      return <TouchableOpacity key={fn.whiteKeyId} style={[styles.whiteKey,{touchAction: "none", height: Dimensions.get('window').height}]} onLayout={this.onLayout}><View/></TouchableOpacity>;
+    });
+    let keyboard2 = pianoKeys.map((fn,i) => {
+      if (fn.blackKeyId) {
+        let w = this.state.calculatedKeyWidth;
+        return <TouchableOpacity activeOpacity={0.5} key={fn.whiteKeyId} style={[styles.blackKey, {left: i*w - w/4, width: w/2}]} ><View/></TouchableOpacity>;
+      }
+    });
+    return (
+      <View style={{flow:1}}>
+        <ScrollView horizontal={true}>
+        {keyboard}
+        {keyboard2}
+        </ScrollView>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF',
-    },
-    welcome: {
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10,
-    },
-    instructions: {
-        textAlign: 'center',
-        color: '#333333',
-        marginBottom: 5,
-    },
+  container: {
+    flexDirection: "row",
+    flex: 1,
+  },
+  whiteKey: {
+    flex: 1,
+    backgroundColor: "white",
+    borderColor: "gray",
+    borderWidth: 1,
+    width: 50
+},
+  blackKey: {
+    backgroundColor: "black",
+    height: "50%",
+    top: 0,
+    position: "absolute"
+  },
 });
